cb8d74c0fe356ff51c21b88767cceaf00e445477
Mr. Marco Prins <snirpocram@gmail.com>
2020-06-28 15:19:35 +0200

    Add victory check

--- app/components/game.js
+++ app/components/game.js
@@ -4,6 +4,14 @@ import { tracked } from '@glimmer/tracking';
 
 const playerTokens = ["x", "o"];
 
+const flipBoard = (board) => {
+  let reversedBoard = board.map(arr => arr.slice()).reverse();
+
+  return reversedBoard[0].map((column, index) => (
+    reversedBoard.map(row => row[index])
+  ))
+};
+
 export default class GameComponent extends Component {
   @tracked activePlayer = 0;
   @tracked board = [
@@ -24,8 +32,15 @@ export default class GameComponent extends Component {
 
   registerMove(row, column) {
     this.updateBoard(row, column);
-    this.checkVictoryCondition();
+    this.checkForGameEnd();
     this.toggleActivePlayer();
+  }
+
+  // Null for draw
+  registerVictory(token) {
+    token !== null ?
+      alert(`Player ${token} has won!`) :
+      alert("It's a draw. Play again");
   }
 
   updateBoard(row, column) {
@@ -38,8 +53,29 @@ export default class GameComponent extends Component {
     this.activePlayer = (this.activePlayer + 1) % playerTokens.length;
   }
 
-  checkVictoryCondition() {
-    // TODO
+  checkForGameEnd() {
+    const token = this.activePlayerToken;
+    if (this.didWin(token)) { this.registerVictory(token) }
+  }
+
+  didWin(token) {
+    const board = this.board;
+    const rotatedBoard = flipBoard(this.board);
+
+    const horizontal =   this.detectHorizontalRows(board, token);
+    const diagonalUp =   this.detectDiagonalRow(board, token);
+    const vertical =     this.detectHorizontalRows(rotatedBoard, token);
+    const diagonalDown = this.detectDiagonalRow(rotatedBoard, token);
+
+    return (horizontal || vertical || diagonalUp || diagonalDown);
+  }
+
+  detectHorizontalRows(board, token) {
+    return board.some(row => row.every(entry => entry === token));
+  }
+
+  detectDiagonalRow(board, token) {
+    return [board[0][0], board[1][1], board[2][2]].every(entry => entry === token);
   }
 
   get activePlayerToken() {
